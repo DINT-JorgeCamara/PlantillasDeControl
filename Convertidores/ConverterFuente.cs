5b825e97679e76d04e703b2dd172a40bb643d509
JorgeCamaraH <jorge.camara@iesdoctorbalmis.com>
2022-11-21 12:45:52 +0100

    Agregar archivos de proyecto.

---
+++ Convertidores/ConverterFuente.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Comida.Convertidores
+{
+    class ConverterFuente : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string fuente = "";
+            switch (value.ToString())
+            {
+                case "Americana":
+                    fuente = "Slim Summer";
+                    break;
+
+                case "China":
+                    fuente = "Chinese Wok Food St";
+                    break;
+
+                case "Mexicana":
+                    fuente = "Taco Salad";
+                    break;
+            }
+            return new FontFamily(fuente);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MainWindowVM.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comida
+{
+    class MainWindowVM : INotifyPropertyChanged
+    {
+        private ObservableCollection<Plato> platos;
+
+        public ObservableCollection<Plato> Platos
+        {
+            get => platos;
+            set
+            {
+                this.platos = value;
+                this.NotifyPropertyChanged("Platos");
+
+            }
+        }
+
+        private ObservableCollection<String> tipos;
+
+        public ObservableCollection<String> Tipos
+        {
+            get => tipos;
+            set
+            {
+                this.tipos = value;
+                this.NotifyPropertyChanged("Tipos");
+
+            }
+        }
+
+        private Plato platoSeleccionado;
+
+        public Plato PlatoSeleccionado
+        {
+            get { return platoSeleccionado; }
+            set
+            {
+                platoSeleccionado = value;
+                NotifyPropertyChanged("PlatoSeleccionado");
+            }
+        }
+
+        public MainWindowVM(){
+            Platos = Plato.GetSamples("C:/Users/alumno/Pictures/ImagenesComida");
+            Tipos = new ObservableCollection<string> { "Americana", "China", "Mexicana" };
+        }
+
+        public void LimpiaSeleccion() {
+            PlatoSeleccionado = null;
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void NotifyPropertyChanged(string propertyName)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
